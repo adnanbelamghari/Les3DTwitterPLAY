9b173536bd88efe7b36ca3bf315978b0a28b7eda
hdomingo <hdomingo@si95.etu.info-ufr.univ-montp2.fr>
2012-11-12 15:13:34 +0100

    new MàJ

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,74 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.Form;
+import play.libs.Json;
+
+
 
 import views.html.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+
+import models.creationCompte;
+
+import org.codehaus.jackson.node.ObjectNode;
+
 public class Application extends Controller {
   
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
+    static Form<creationCompte> taskForm = form(creationCompte.class);
+    //static Form<Project> projectForm = form(Project.class);
   
+  
+    /* public static Result index() {
+	    return ok(index.render("Bienvenu dans votre application 3DTWITTER."));
+    }	*/    
+    
+
+	public static Result index() {
+		return redirect(routes.Application.creationComptes());
+	}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
